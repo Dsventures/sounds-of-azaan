d63bb1f18c5b13822170342b91e2f261618fa9ba
Eunice Dhivya <divi.seed@gmail.com>
2022-05-22 20:32:07 +0530

    add other font formats

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 new fullpage("#fullpage", {
   sectionsColor: ["#add8d7", "#add8d7"],
 });
+
+var cities = document.getElementsByClassName("cities");
+
+for (var i = 0; i < cities.length; i++) {
+  cities[i].addEventListener("click", function (e) {
+    console.log(this.id);
+  });
+}
+
+var dataset = {};
