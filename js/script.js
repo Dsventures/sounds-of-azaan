7fb727ad1d1ea2b26d39be8853a5b68dd82db2aa
Eunice Dhivya <divi.seed@gmail.com>
2022-05-23 16:16:44 +0530

    add progress bar to audio player

--- js/script.js
+++ js/script.js
@@ -4,15 +4,24 @@ new fullpage("#fullpage", {
 });
 
 var cities = document.getElementsByClassName("cities");
+var cityName = document.getElementById("cityName");
+var audio = document.getElementById("audio");
+var playPauseBtn = document.getElementById("playAudio");
 
 for (var i = 0; i < cities.length; i++) {
   cities[i].addEventListener("click", function (e) {
     console.log(this.id);
+    cityName.innerHTML = this.id;
+    audio.src = "sound/" + this.id + ".mp3";
+    document.getElementsByClassName("modelbox")[0].style.display = "block";
+    playPause();
   });
 }
+document.getElementById("closeBtn").addEventListener("click", function () {
+  document.getElementsByClassName("modelbox")[0].style.display = "none";
+  stop();
+});
 
-var audio = document.getElementById("audio");
-var playPauseBtn = document.getElementById("playAudio");
 var count = 0;
 
 function playPause() {
@@ -33,4 +42,16 @@ function stop() {
   count = 0;
 }
 
-var dataset = {};
+var progressed = document.getElementById("progressed");
+
+audio.ontimeupdate = function () {
+  console.log("playing", (100 * audio.currentTime) / audio.duration);
+  progressed.style.width = (100 * audio.currentTime) / audio.duration + "%";
+};
+
+var dataset = {
+  Kochi: {
+    state: "Kerala",
+    desc: "",
+  },
+};
