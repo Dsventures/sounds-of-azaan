55281c344089b35ff36a2932ede0314d6e0db2ba
Eunice Dhivya <divi.seed@gmail.com>
2022-05-25 16:29:02 +0530

    four corner images

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 new fullpage("#fullpage", {
   licenseKey: "29D22B9D-244C4C74-87A11080-BE68FCC1",
-  sectionsColor: ["#add8d7", "#add8d7"],
 });
 
 var cities = document.getElementsByClassName("cities");
